1f89643f82bccd1306e8c4da13ea42cc59b405d5
Alexey-Zhuk <a.zhuk@sellfree.by>
2020-01-15 12:50:25 +0300

    Maven task. Change Junit ver. 4.12 ->4.11

--- Java/src/main/java/com/github/vitalliuss/helloci/EmptyClass.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/EmptyClass.java
@@ -1,10 +1,5 @@
 package com.github.vitalliuss.helloci;
 
-/**
- * Empty class for Cobertura code coverage
- * @author vitali_shulha
- *
- */
 public class EmptyClass {
 	
 	public static void emptyMethod() {
